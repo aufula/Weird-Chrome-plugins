05a50af49e823ceba71852f3d4f1f0431d5717a8
灰目 <aufula@hotmail.com>
2013-09-13 23:57:46 +0800

    Update fc2movie.user.js

--- fc2movie.user.js
+++ fc2movie.user.js
@@ -6,11 +6,11 @@
 // @match *://modimovie.com/*
 // ==/UserScript==
 
-window.onload = function(){
+(function(){
     var embed = document.querySelector('embed#flv2id');
     var obj = embed.parentNode;
     embed.src = embed.src.replace(/flv3/,'flv2');
     embed.style.display='block';
     obj.parentNode.insertBefore(embed,obj.nextSibling);
     obj.remove();
-};
+})();

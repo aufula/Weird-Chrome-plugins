122980cd6b47be252925701f8697e86e7fd6792a
aufula <aufula@hotmail.com>
2014-05-28 05:54:41 -0400

    update livereload

--- livereload.user.js
+++ livereload.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name livereload
 // @description livereload 插件
-// @version 2.0.1
+// @version 2.0.2
 // @include *//dev*
 // ==/UserScript==
 
 (function(){
     var script = document.createElement('script');
-    script.src="//dev:35729/livereload.js";
+    //var path =  '//' + location.host;
+    var path=  "https://dl.dropboxusercontent.com/u/35000411";
+    script.src = path + "/livereload.js";
     document.head.insertBefore(script, document.head.firstChild);
 })();
